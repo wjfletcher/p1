ebb8d9aac67f1ff873c20c4af3550c7c1527c8ff
Warren Fletcher <warrenjfletcher@gmail.com>
2015-09-16 12:44:59 -0400

    Added sections for questions about me

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,57 +12,80 @@
     <title>Starter Template for Bootstrap</title>
 
     <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 
-<!-- Latest compiled and minified JavaScript -->
+    <link rel="stylesheet" href="css/custom.css">
 
 
-    
-  </head>
 
-  <body>
+</head>
+
+<body>
 
     <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Warren Fletcher</a>
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Warren Fletcher</a>
+            </div>
+            <div id="navbar" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#p1">P1</a></li>
+                    <li><a href="#p2">P2</a></li>
+                    <li><a href="#p3">P3</a></li>
+                    <li><a href="#p4">P4</a></li>
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
         </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#p1">P1</a></li>
-            <li><a href="#p2">P2</a></li>
-            <li><a href="#p3">P3</a></li>
-            <li><a href="#p4">P4</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
     </nav>
-
+    <br>
+    <br>
+    <br>
     <div class="container">
+        <div class="jumbotron">
+            <h1>Warren Fletcher</h1>
+            <h2>Dynamic Web Applications Portfolio</h2>
+            <br>
+            <img src="img/me.jpg">
+        </div>
+        <div class="section">
+            <h1>In 2+ sentences describe who you are and what you do.</h1>
+            <p>adskljhas;ldh</p>
+        </div>
+        <div class="section">
+            <h1>Why are you taking this course?</h1>
+            <p>adskljhas;ldh</p>
+        </div>
+        <div class="section">
+            <h1>What previous experience do you have with programming?</h1>
+            <p>adskljhas;ldh</p>
+        </div>
+        <div class="section">
+            <h1>Are you working on a Mac, PC, or some other platform?</h1>
+            <p>adskljhas;ldh</p>
+        </div>
+        <div class="section">
+            <h1>Anything else we should know?</h1>
+            <p>adskljhas;ldh</p>
+        </div>
+    </div>
 
-      <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-      </div>
-
-    </div><!-- /.container -->
-
-
-    <!-- Bootstrap core JavaScript
+            <!-- Bootstrap core JavaScript
     ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    
-  </body>
+            <!-- Placed at the end of the document so the pages load faster -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+</body>
+
 </html>
